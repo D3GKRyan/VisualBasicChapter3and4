a7ecf83775ac377a1b923ecba7452cbaa690b1ab
DGGKRyan <ryanc18dg@gmail.com>
2017-11-03 11:18:49 -0500

    dateString and txtControl

---
+++ dateString/dateString/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtDayOfWeek.TextChanged
+
+    End Sub
+
+    Private Sub btnShowDate_Click(sender As Object, e As EventArgs) Handles btnShowDate.Click
+        lblDateString.Text = txtDayOfWeek.Text & ", " & txtMonth.Text & " " & txtDayOfMonth.Text & " , " & txtYear.Text
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtDayOfWeek.Clear()
+        txtMonth.Clear()
+        txtDayOfMonth.Clear()
+        txtYear.Clear()
+        lblDateString.Text = String.Empty
+        txtMonth.Focus()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+End Class
---
+++ textBoxControl/textBoxControl/Form1.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.btnShowGreeting = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblGreeting = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(69, 61)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Your Name"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(48, 77)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(138, 20)
+        Me.txtUserName.TabIndex = 1
+        '
+        'btnShowGreeting
+        '
+        Me.btnShowGreeting.Location = New System.Drawing.Point(39, 138)
+        Me.btnShowGreeting.Name = "btnShowGreeting"
+        Me.btnShowGreeting.Size = New System.Drawing.Size(75, 40)
+        Me.btnShowGreeting.TabIndex = 2
+        Me.btnShowGreeting.Text = "Show Greeting"
+        Me.btnShowGreeting.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(120, 138)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 40)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblGreeting
+        '
+        Me.lblGreeting.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGreeting.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblGreeting.Location = New System.Drawing.Point(66, 101)
+        Me.lblGreeting.Name = "lblGreeting"
+        Me.lblGreeting.Size = New System.Drawing.Size(100, 25)
+        Me.lblGreeting.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblGreeting)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnShowGreeting)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Greetings"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtUserName As System.Windows.Forms.TextBox
+    Friend WithEvents btnShowGreeting As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblGreeting As System.Windows.Forms.Label
+
+End Class
